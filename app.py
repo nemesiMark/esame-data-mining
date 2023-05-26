9bb4ce49dae12bdf5e609ca5e2697e3feffdc7ea
nemesiMark <marcantonio.cilia@gmail.com>
2023-05-26 10:57:58 +0200

    first push

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+from sklearn.decomposition import PCA
 import joblib
 
 
